93d6a29b6944f28128163ed41e9dcf9ca7b74e8c
Matthew Colson <colson78@live.missouristate.edu>
2013-03-05 23:25:21 -0600

    added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+	# Returns the full title on a per-page basis.
+	# It will return a formates title even if page
+	# does not supply one.
+	
+	# Define class named full_title
+	def full_title(page_title)
+		
+		# Will be used a default title
+		base_title = "Ruby on Rails Tutorial Sample App"
+
+		# If page title is empty base title will be used
+		if page_title.empty?
+			base_title
+		# Else base_title and page title will be formated together
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
