4ea364457edca58fcf4b710949ee1d8e0d891af2
Matthew Colson <colson78@live.missouristate.edu>
2013-03-06 20:52:09 -0600

    finish chapter six

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,17 +1,3 @@
-# Method used in testing that the title is correct
-def full_title(page_title)
-	
-	# The default title each page should have
-	base_title = "Ruby on Rails Tutorial Sample App"
-
-	# If page_title is empty then base title is used.
-	if page_title.empty?
-		base_title
-	# If page_title is not empty than it is added to the
-	# base_title to make a custome title.
-	else
-		# Formats the title with both the base_title and the
-		# page_title with | in between.
-		"#{base_title} | #{page_title}"
-	end
-end
+# Give access to the application_helper_spec.rd file
+# and the ApplicationHelper.
+include ApplicationHelper
