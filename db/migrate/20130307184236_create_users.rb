c71beae0d36e9e29a014e4c98ca8cd77ce41a861
Matthew Colson <colson78@live.missouristate.edu>
2013-03-07 13:02:27 -0600

    add documentation to migrate file

--- db/migrate/20130307184236_create_users.rb
+++ db/migrate/20130307184236_create_users.rb
@@ -1,9 +1,22 @@
+# Create table user with attributes name and email
+# both of the type string.
 class CreateUsers < ActiveRecord::Migration
+  # Making change to database
   def change
+    # Create table user
+    # The t variable is a representation of table
+    # Users, the pural convention is used to represent that
+    # the table will hold many users.
     create_table :users do |t|
+      # Create attribute name of type string for t variable
       t.string :name
+
+      # Create attribute email of type string for t variable
       t.string :email
 
+      # Two special attributes, created_at and updated_at,
+      # are automatically created and givin the type datetime
+      # with the currnt time.
       t.timestamps
     end
   end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,16 +1,28 @@
+# Gives file access to spec_helper.rb
 require 'spec_helper'
 
+# Name used to access the test from another file
 describe ApplicationHelper do
 
+  # When ever full_title is used in test these
+  # test will be run
   describe "full_title" do
+    # Test that if full_title have string passed
+    # into it then that string should be in the
+    # title.
     it "should include the page title" do
       full_title("foo").should =~ /foo/
     end
 
+    # Test that it full title has string passed
+    # into it then "Ruby on Rails Tutorial Sample App"
+    # should be in the title.
     it "should include the base title" do
       full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
     end
 
+    # Test that if full_title is not passed a string in
+    # then the | should not be in the title.
     it "should not include a bar for the home page" do
       full_title("").should_not =~ /\|/
     end
