c81a153ecc1942643244808f3bdb5b27067d7de8
Matthew Colson <colson78@live.missouristate.edu>
2013-03-10 19:16:18 -0500

    add show action to users controller and creates show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def new
   end
+
+  # Show action for users.
+  # Used to get and display user infromation
+  # from database.
+  def show
+  	# Creates a @user variable that populates it
+  	# with database query using the id attribute
+  	# from the user table.
+  	@user = User.find(params[:id])
+  end
 end
