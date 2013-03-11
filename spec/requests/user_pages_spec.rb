9023fc2b0ba28e41d9f02a7e71e32041ed96337b
Matthew Colson <colson78@live.missouristate.edu>
2013-03-10 19:33:36 -0500

    add profile page test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,4 +21,20 @@ describe "UserPages" do
 		# the content Sign Up.
 		it { should have_selector 'title', text: full_title('Sign up') }
 	end
+
+	# Test are for the profile page
+	describe "- Profile page" do
+		
+		# Before each test visit user_path route passing in
+		# user variable as parameter.
+		before { visit user_path(user) }
+
+		# Test that the page has a h1 tag with the users name
+		# populated within it.
+		it { should have_selector('h1', text: user.name) }
+
+		# Test that the page has a title tag with the users name
+		# populated within it.
+		it { should have_selector('title', text: user.name) }
+	end
 end
