9268f100a9140e95ae5af7e3f8fb8d1995677372
Matthew Colson <colson78@live.missouristate.edu>
2013-03-13 23:54:54 -0500

    update sessions_controller for successful signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,28 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+	    
+	    # Get user from database using the email from the sessions hash.
+	    # Downcase it to ensure no case issues arise.
 	    user = User.find_by_email(params[:session][:email].downcase)
 
+	    # Test to see it user exist and that password matches using
+	    # the authenticate method by pulling encrypted hash from database
+	    # and testing it to the encrtpeted hash created from the password
+	    # stored in the session hash.
 	    if user && user.authenticate(params[:session][:password])
-	    # Sign the user in and redirect to the user's show page.
+	    	# Call method sign_in passing user in a parameter to sing in the user
+	    	sign_in user
+
+	    	# Redirect to the profile page for the user
+	    	redirect_to user
+	    # If authentication failes code is executed
 	    else
-	      flash[:error] = 'Invalid email/password combination' # Not quite right!
+	      # Flash now generated a error message that will not persist
+	      # if link if click instead of submit.
+	      flash.now[:error] = 'Invalid email/password combination'
+
+	      # Reload the page
 	      render 'new'
 	    end
 	end
