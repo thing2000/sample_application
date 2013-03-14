79850ef7f450ba2ba2e3dcff40edae9ce29f30b8
Matthew Colson <colson78@live.missouristate.edu>
2013-03-13 22:30:45 -0500

    added signin page test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
-  describe "GET /authentication_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get authentication_pages_index_path
-      response.status.should be(200)
-    end
-  end
+# Test for authentication pages
+describe "Authentication" do
+	
+	# Lets test know that the subject of the test will always
+	# be the page.
+	subject { page }
+
+	# Test for the signing page
+	describe "signin page" do
+		
+		# Before each test visit the singin page
+		before { visir signin_path }
+
+		# The signin page should have a h1 tag containing the text
+		# Sign in.
+		it { should have_selector('h1', text: 'Sign in') }
+		
+		# The signin page should have a title tag containing the text
+		# Sign in.
+		it { should have_selector('title', text: 'Sign in') }
+	end
 end
