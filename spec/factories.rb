c5ae5cfb74cae71e9d5913c2ed4225ed2e29e325
Matthew Colson <colson78@live.missouristate.edu>
2013-03-15 21:21:39 -0500

    update factory to generate unique names and email

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
 	factory :user do
-		name "Matthew Colson"
-		email "colson78@live.missouristate.edu"
+
+		# Will cycle through names every time its called thus
+		# creating a unique name
+		sequence(:name)  { |n| "Person #{n}" }
+
+		# Will cycle through email every time its called thus
+		# creating a unique email
+    	sequence(:email) { |n| "person_#{n}@example.com"}  
 		password "password"
 		password_confirmation "password"
 	end
