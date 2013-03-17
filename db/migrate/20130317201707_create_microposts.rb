9a5857924b08828e20d3ccd62abd770d28cbf0d5
Matthew Colson <colson78@live.missouristate.edu>
2013-03-17 15:37:13 -0500

    update micropost test

--- db/migrate/20130317201707_create_microposts.rb
+++ db/migrate/20130317201707_create_microposts.rb
@@ -1,10 +1,22 @@
 class CreateMicroposts < ActiveRecord::Migration
+  # Change the database
   def change
+
+  	# Create a table micropost assign it to block variable t
     create_table :microposts do |t|
+
+      # Create attribute content with type string
       t.string :content
+
+      # Create a attriburte user_id with type integer
       t.integer :user_id
 
+      # used to assign value for created_at and updated_at
       t.timestamps
     end
+
+    # Add index with user_id and created_at as keys to array
+    # This will allow quick search of most recent entries by user
+    add_index :microposts, [:user_id, :created_at]
   end
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,45 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
+# Test describe the micropost model
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # Create a new user object and assign to symbol user
+  let(:user) { FactoryGirl.create(:user) }
+  
+  # Before each of the following test
+  before do
+    # This code is wrong!
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+  end
+
+  # Tells test that @micropost will be the subject of the test
+  subject { @micropost }
+
+  # Test that content is a attribute in the micropost table
+  it { should respond_to(:content) }
+
+  # Test that user_id is a attribut in the micropost table
+  it { should respond_to(:user_id) }
+
+  # Test for missing user_id
+  describe "- When user_id is not present" do
+    
+  	# Sets the user_id to nil in @micropost object
+    before { @micropost.user_id = nil }
+
+    # Insure that @micropost is no a valid object any longer
+    it { should_not be_valid }
+  end
 end
+
