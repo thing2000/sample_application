6723a938235f94b2e5d3cdb64607c36280bbff1a
Matthew Colson <colson78@live.missouristate.edu>
2013-03-17 22:20:26 -0500

    update micropost pages test

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,11 +1,57 @@
 require 'spec_helper'
 
-describe "MicropostPages" do
-  describe "GET /micropost_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get micropost_pages_index_path
-      response.status.should be(200)
+# Test for micropost pages
+describe "Micropost pages" do
+
+  # Tells rails page is subject of test
+  subject { page }
+
+  # Generate user object for test
+  let(:user) { FactoryGirl.create(:user) }
+  
+  # Signin user
+  before { sign_in user }
+
+  # Test creation of micropost
+  describe "micropost creation" do
+    
+    # Simulate visit to root path
+    before { visit root_path }
+
+    # Test for when invalid information is entered
+    describe "with invalid information" do
+
+      # Test that micropost was not created
+      it "should not create a micropost" do
+        
+        # Simulate click submit and count micropost to see if count
+        # has not changed.
+        expect { click_button "Post" }.not_to change(Micropost, :count)
+      end
+
+      # Test for error message
+      describe "error messages" do
+        
+        # Simulate a button click
+        before { click_button "Post" }
+
+        # Page should have an error message on it
+        it { should have_content('error') } 
+      end
+    end
+
+    # Test for valid entry of information
+    describe "with valid information" do
+
+      # Fill in form with valid information
+      before { fill_in 'micropost_content', with: "Lorem ipsum" }
+
+      # Test that micropost is created
+      it "should create a micropost" do
+
+        # Sumilate button click and count micropost to see that one was added
+        expect { click_button "Post" }.to change(Micropost, :count).by(1)
+      end
     end
   end
 end
