bef1b1c4052d12a75218b530274c8a65405cceb7
Matthew Colson <colson78@live.missouristate.edu>
2013-03-18 16:01:52 -0500

    updated error message partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,10 @@ class StaticPagesController < ApplicationController
   
   # Controller class for home page
   def home
+
+    # Builds the micropost for current user and assign it to
+    # instance variable @micropost.
+    @micropost = current_user.microposts.build if signed_in?
   end
 
   # Controller class for the help page
