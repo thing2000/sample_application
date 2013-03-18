a1418b0625f1df10775d3efe923e702bda8b2199
Matthew Colson <colson78@live.missouristate.edu>
2013-03-17 22:26:27 -0500

    update microposts_controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,15 +1,18 @@
 class MicropostsController < ApplicationController
   
   # If user is not signed in redirect them to signin page.
-  # It will only be applied to create and destroy actions.
-  before_filter :signed_in_user, only: [:create, :destroy]
-
-  def index
-  end
+  before_filter :signed_in_user
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
-
+  
   def destroy
   end
 end
