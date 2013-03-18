1ff0ddb50bb2dfa45da158e9f5c4bed8eb377958
Matthew Colson <colson78@live.missouristate.edu>
2013-03-18 16:52:14 -0500

    update static_pages_controller with feed_items

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,9 @@ class StaticPagesController < ApplicationController
     # Builds the micropost for current user and assign it to
     # instance variable @micropost.
     @micropost = current_user.microposts.build if signed_in?
+
+    # Builds the micropost for current user based on their feed attribute
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   # Controller class for the help page
